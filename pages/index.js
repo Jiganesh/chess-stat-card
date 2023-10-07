47730a14bb647f358c8b59dff6c91e71f6b40491
Jiganesh <jiganeshpatil01071999@gmail.com>
2023-10-08 01:22:10 +0530

    Fix : Rating change and added default values

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
+
+const NOAVATAR = "https://www.chess.com/bundles/web/images/noavatar_l.84a92436.gif"
+const DEFAULT_LOCATION = "Somewhere on Earth"
+
 async function get_stats(){
 
   try{
   const inputfieldname = document.getElementById("username")
   const username = inputfieldname.value
 
-  const player_details_url = `https://api.chess.com/pub/player/${username}`
+  const player_details_url = `https://api.chess.com/pub/player/${username}/`
   const player_details_response = await fetch(player_details_url)
 
   const player_details = await player_details_response.json(); 
@@ -14,27 +18,27 @@ async function get_stats(){
   document.querySelector("#avatar_name").innerHTML = player_details.name
 
 
-  const player_stats_url = `https://api.chess.com/pub/player/${username}/stats`
+  const player_stats_url = `https://api.chess.com/pub/player/${username}/stats/`
   const player_stats_response = await fetch(player_stats_url)
 
   const player_stats = await player_stats_response.json(); 
 
-  const rapid_best_stats = player_stats.chess_rapid.best
-  const blitz_best_stats = player_stats.chess_blitz.best
-  const bullet_best_stats = player_stats.chess_bullet.best
-  const daily_best_stats = player_stats.chess_daily.best
+  const rapid_best_stats =  (player_stats.hasOwnProperty("chess_rapid")) ? player_stats.chess_rapid.best["rating"]  : "Not Available"
+  const blitz_best_stats =  (player_stats.hasOwnProperty("chess_blitz")) ? player_stats.chess_blitz.best["rating"] : "Not Available"
+  const bullet_best_stats = (player_stats.hasOwnProperty("chess_bullet"))? player_stats.chess_bullet.best["rating"] : "Not Available"
+  const daily_best_stats =  (player_stats.hasOwnProperty("chess_daily")) ? player_stats.chess_daily.best ["rating"] : "Not Available"
 
 
   let dictionary = {
-    photo : player_details.avatar,
+    photo : (player_details.avatar == undefined) ? NOAVATAR: player_details.avatar,
     name : player_details.name,
     username: player_details.username,
-    location: player_details.location,
+    location: (player_details.location == undefined) ? DEFAULT_LOCATION : player_details.location ,
     league : player_details.league,
-    rapid_best : rapid_best_stats["rating"],
-    blitz_best : blitz_best_stats["rating"],
-    bullet_best: bullet_best_stats["rating"],
-    daily_best:daily_best_stats["rating"]
+    rapid_best : rapid_best_stats,
+    blitz_best : blitz_best_stats,
+    bullet_best: bullet_best_stats,
+    daily_best:daily_best_stats
   }
 
   document.querySelector("#avatar_image").src = dictionary.photo
@@ -48,10 +52,9 @@ async function get_stats(){
   document.querySelector("#avatar_location").innerHTML = "Location :" + dictionary.location
 
 
-  console.log(dictionary)
-
-  }catch{
-    console.log("runtime error")
+  console.log(dictionary)}
+  catch{
+    console.log("SOMETHING WENT WRONG AHHHHHHHHHH")
   }
 
 }
@@ -60,18 +63,18 @@ function Chess(){
 
   return (
       <div>
-          <label>Name</label>
+          <label>Provide username here : </label>
           <input type="text" name="name" id="username" onChange={get_stats}/> <br></br>
 
-          <img src="" id="avatar_image"></img>  <br></br>
-          <p1 id="avatar_name"></p1> <br></br>
-          <p1 id="avatar_username"></p1> <br></br>
-          <p1 id="avatar_location"></p1> <br></br>
-          <p1 id="avatar_league"></p1> <br></br>
-          <p1 id="avatar_rapid"></p1> <br></br>
-          <p1 id="avatar_blitz"></p1> <br></br>
-          <p1 id="avatar_bullet"></p1> <br></br>
-          <p1 id="avatar_daily"></p1> <br></br>
+          <img src={NOAVATAR} id="avatar_image"></img>  <br></br>
+          <p1 id="avatar_name">Name</p1> <br></br>
+          <p1 id="avatar_username">Username</p1> <br></br>
+          <p1 id="avatar_location">Location</p1> <br></br>
+          <p1 id="avatar_league">League</p1> <br></br>
+          <p1 id="avatar_rapid">Rapid Rating</p1> <br></br>
+          <p1 id="avatar_blitz">Blitz Rating</p1> <br></br>
+          <p1 id="avatar_bullet">Bullet Rating</p1> <br></br>
+          <p1 id="avatar_daily">Daily Rating</p1> <br></br>
 
 
           
